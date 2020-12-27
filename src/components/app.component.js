14d0af68ca4bf9204782d046e8791c8693c3e1a7
YellowFlash525 <magicproject525@gmail.com>
2020-12-27 22:20:37 +0100

    Code refactoring

--- src/components/app.component.js
+++ src/components/app.component.js
@@ -10,44 +10,44 @@ import NightSky from './nightSky/nightSky.component';
 import ThanosPortal from './thanosPortal/thanosPortal.component';
 import SolarSystem from './solarSystem/solarSystem.component';
 
-export default function App() {
-    return (
-        <Router>
-          <div>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/night-sky">Night Sky</Link>
-                </li>
-                <li>
-                  <Link to="/thanos-portal">Thanos portal</Link>
-                </li>
-                <li>
-                  <Link to="/solar-system">Solar-System</Link>
-                </li>
-              </ul>
-            </nav>
-    
-            {/* A <Switch> looks through its children <Route>s and
-                renders the first one that matches the current URL. */}
-            <Switch>
-              <Route path="/night-sky">
-                <NightSky />
-              </Route>
-              <Route path="/thanos-portal">
-                <ThanosPortal />
-              </Route>
-              <Route path="/solar-system">
-                <SolarSystem />
-              </Route>
-              <Route path="/">
-                <Home />
-              </Route>
-            </Switch>
-          </div>
-        </Router>
-      );
-};
+const App = () => (
+	<Router>
+		<div>
+		<nav>
+			<ul>
+			<li>
+				<Link to="/">Home</Link>
+			</li>
+			<li>
+				<Link to="/night-sky">Night Sky</Link>
+			</li>
+			<li>
+				<Link to="/thanos-portal">Thanos portal</Link>
+			</li>
+			<li>
+				<Link to="/solar-system">Solar-System</Link>
+			</li>
+			</ul>
+		</nav>
+
+		{/* A <Switch> looks through its children <Route>s and
+			renders the first one that matches the current URL. */}
+		<Switch>
+			<Route path="/night-sky">
+			<NightSky />
+			</Route>
+			<Route path="/thanos-portal">
+			<ThanosPortal />
+			</Route>
+			<Route path="/solar-system">
+			<SolarSystem />
+			</Route>
+			<Route path="/">
+			<Home />
+			</Route>
+		</Switch>
+		</div>
+	</Router>
+);
+
+export default App;
