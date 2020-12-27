6ae9fc6eba64adf1d1e907ca238e7c555b9ea943
YellowFlash525 <magicproject525@gmail.com>
2020-12-27 22:25:20 +0100

    Code refactoring

--- src/components/app.component.js
+++ src/components/app.component.js
@@ -13,39 +13,36 @@ import SolarSystem from './solarSystem/solarSystem.component';
 const App = () => (
 	<Router>
 		<div>
-		<nav>
-			<ul>
-			<li>
-				<Link to="/">Home</Link>
-			</li>
-			<li>
-				<Link to="/night-sky">Night Sky</Link>
-			</li>
-			<li>
-				<Link to="/thanos-portal">Thanos portal</Link>
-			</li>
-			<li>
-				<Link to="/solar-system">Solar-System</Link>
-			</li>
-			</ul>
-		</nav>
-
-		{/* A <Switch> looks through its children <Route>s and
-			renders the first one that matches the current URL. */}
-		<Switch>
-			<Route path="/night-sky">
-			<NightSky />
-			</Route>
-			<Route path="/thanos-portal">
-			<ThanosPortal />
-			</Route>
-			<Route path="/solar-system">
-			<SolarSystem />
-			</Route>
-			<Route path="/">
-			<Home />
-			</Route>
-		</Switch>
+			<nav>
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/night-sky">Night Sky</Link>
+					</li>
+					<li>
+						<Link to="/thanos-portal">Thanos portal</Link>
+					</li>
+					<li>
+						<Link to="/solar-system">Solar-System</Link>
+					</li>
+				</ul>
+			</nav>
+			<Switch>
+				<Route path="/night-sky">
+					<NightSky />
+				</Route>
+				<Route path="/thanos-portal">
+					<ThanosPortal />
+				</Route>
+				<Route path="/solar-system">
+					<SolarSystem />
+				</Route>
+				<Route path="/">
+					<Home />
+				</Route>
+			</Switch>
 		</div>
 	</Router>
 );
